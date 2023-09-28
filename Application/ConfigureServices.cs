a65943b5831686c33be41efbfe268568d180ecbb
Aldmi1988 <aldmi@bk.ru>
2023-09-28 19:57:39 +0700

    Init

---
+++ Application/ConfigureServices.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+        // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+
+        return services;
+    }
+
+    // public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    // {
+    //     if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+    //     {
+    //         services.AddDbContext<ApplicationDbContext>(options =>
+    //             options.UseInMemoryDatabase("VerticalSliceDb"));
+    //     }
+    //     else
+    //     {
+    //         services.AddDbContext<ApplicationDbContext>(options =>
+    //             options.UseSqlServer(
+    //                 configuration.GetConnectionString("DefaultConnection"),
+    //                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+    //     }
+    //
+    //     services.AddScoped<IDomainEventService, DomainEventService>();
+    //
+    //     services.AddTransient<IDateTime, DateTimeService>();
+    //     services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+    //
+    //     services.AddSingleton<ICurrentUserService, CurrentUserService>();
+    //
+    //     return services;
+    // }
+
+
+    // public static IServiceCollection AddRabitMqConsumers(this IServiceCollection services, IConfiguration configuration)
+    // {
+    //     services.Configure<RabbitMqConfiguration>(configuration);
+    //     services.AddSingleton<IRabbitMqService, RabbitMqService>();
+    //     services.AddSingleton<IConsumerService, CreateSoundFileConsumerService>();
+    //     services.AddHostedService<ConsumerHostedService>();
+    //     
+    //     return services;
+    // }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+    policy => policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()));
+
+
+builder.Services.AddHealthChecks();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseHttpsRedirection();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+    
+    app.UseExceptionHandler("/error-development");
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
