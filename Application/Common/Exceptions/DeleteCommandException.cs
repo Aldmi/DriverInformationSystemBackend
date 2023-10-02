f40551023537c325987de08d8525af9647ffb4ac
Aldmi1988 <aldmi@bk.ru>
2023-10-02 13:36:40 +0700

    Add DeleteTrainCommand

---
+++ Application/Common/Exceptions/DeleteCommandException.cs
@@ -0,0 +1,23 @@
+﻿namespace Application.Common.Exceptions;
+
+public class DeleteCommandException : Exception
+{
+    public DeleteCommandException()
+    {
+    }
+
+    public DeleteCommandException(string message)
+        : base(message)
+    {
+    }
+
+    public DeleteCommandException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public DeleteCommandException(string name, object key)
+        : base($"Entity '{name}' ({key}) was not DELETE.")
+    {
+    }
+}
---
+++ Application/Features/TrainLists/DeleteCommand/DeleteTrainCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common;
+using Application.Common.Exceptions;
+using Application.Domain;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Features.TrainLists.DeleteCommand;
+
+
+public class DeleteTrainCommandController : ApiControllerBase
+{
+    [HttpDelete("/api/trains/{id}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        await Mediator.Send(new DeleteTrainCommand { Id = id });
+        return NoContent();
+    }
+}
+
+
+public class DeleteTrainCommand: IRequest
+{
+    public Guid Id { get; set; }
+}
+
+
+internal sealed class DeleteTodoItemCommandHandler : IRequestHandler<DeleteTrainCommand>
+{
+    private readonly ITrainRepository _trainRepository;
+
+    public DeleteTodoItemCommandHandler(ITrainRepository trainRepository)
+    {
+        _trainRepository = trainRepository;
+    }
+
+    public async Task Handle(DeleteTrainCommand request, CancellationToken cancellationToken) //TODO: передавать cancellationToken из Handle
+    {
+        var res= await _trainRepository.DeleteAsync(request.Id);
+        if (!res)
+        {
+            throw new DeleteCommandException(nameof(Train), request.Id); //TODO: Возврат ошибок из Handle через Exception заменить на Result<T>, с обработкой Fail статуса в Middleware
+        }
+    }
+}
