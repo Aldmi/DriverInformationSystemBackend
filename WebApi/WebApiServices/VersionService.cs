a9c51ff50656bff00641aaa98942889b32bfec63
Aldmi1988 <aldmi@bk.ru>
2023-10-17 23:50:51 +0700

    Release_Ver1.0.0

--- WebApi/WebApiServices/VersionService.cs
+++ WebApi/WebApiServices/VersionService.cs
@@ -6,10 +6,10 @@ public static class VersionService
     {
         var aboutProgram = new
         {
-            Ver= "1.0.0",
-            Description= "Add CORS with default Policy",
-            Commit= "b0faecfd702c6dd80e32463741924dcc541b8c83 [b0faecf]",
-            Date = "13 октября 2023г."
+            Ver= "1.0.1",
+            Description= "- Add LogLevel managment. - Load Settings in Production from Enviroment",
+            Commit= "ad51eef8faaf83a7311433a52598e43d7b06264a [ad51eef]",
+            Date = "17 октября 2023г."
         };
        return $"Ver: '{aboutProgram.Ver}'\tDate: '{aboutProgram.Date}'\tDescription: '{aboutProgram.Description}'\t Git: '{aboutProgram.Commit}'\t";
     }
