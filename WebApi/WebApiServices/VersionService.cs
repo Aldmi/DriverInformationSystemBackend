aa108ad7e752346ac3d513a50a7b0a8222061b43
Aldmi1988 <aldmi@bk.ru>
2023-10-27 15:05:15 +0700

    Change ReadMe

--- WebApi/WebApiServices/VersionService.cs
+++ WebApi/WebApiServices/VersionService.cs
@@ -8,7 +8,7 @@ public static class VersionService
         {
             Ver= "1.0.4",
             Description= "Add GetUowListByStationTagQuery and UpdateUowListByStationTagCommand with Validator",
-            Commit= "258acd5f05b0f17c2349bb873b045cdc80cf53a2 [258acd5]",
+            Commit= "3635c44580b86da0087bcea20dc4f52b0d4d1e8 [c3635c4]",
             Date = "27 октября 2023г."
         };
        return $"Ver: '{aboutProgram.Ver}'\tDate: '{aboutProgram.Date}'\tDescription: '{aboutProgram.Description}'\t Git: '{aboutProgram.Commit}'\t";
