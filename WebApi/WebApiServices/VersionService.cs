ca9c33611edbf0131de65c956c09965f9c6051d0
Aldmi1988 <aldmi@bk.ru>
2023-10-28 12:42:18 +0700

    Realese_Ver1.0.5  [Fix bug in UpdateCarrigeListValidator - uniqCarrigeNumber kenght 2..3]

--- WebApi/WebApiServices/VersionService.cs
+++ WebApi/WebApiServices/VersionService.cs
@@ -6,10 +6,10 @@ public static class VersionService
     {
         var aboutProgram = new
         {
-            Ver= "1.0.4",
-            Description= "Add GetUowListByStationTagQuery and UpdateUowListByStationTagCommand with Validator",
-            Commit= "3635c44580b86da0087bcea20dc4f52b0d4d1e8 [c3635c4]",
-            Date = "27 октября 2023г."
+            Ver= "1.0.5",
+            Description= "Fix bug in UpdateCarrigeListValidator - uniqCarrigeNumber kenght 2..3",
+            Commit= "db3dec86d31feae5ef8bd7e08c4d1d1473fcf580 [db3dec8]",
+            Date = "28 октября 2023г."
         };
        return $"Ver: '{aboutProgram.Ver}'\tDate: '{aboutProgram.Date}'\tDescription: '{aboutProgram.Description}'\t Git: '{aboutProgram.Commit}'\t";
     }
